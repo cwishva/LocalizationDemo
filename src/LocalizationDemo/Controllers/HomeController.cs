3ae39e6f4b173ec13e4124a2883cadcfce36a7a9
Chanaka Jayarathna <chanaka.j@evicio.com>
2017-02-13 16:51:08 +0530

    change to make sense

--- src/LocalizationDemo/Controllers/HomeController.cs
+++ src/LocalizationDemo/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace LocalizationDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer<HomeController> localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            this.localizer = localizer;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.HomeController = this.localizer["HomeControllerLocalizer"];
             return View();
         }
 
