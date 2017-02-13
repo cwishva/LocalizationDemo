6ad92747a09fe7ff9b121915ccc5480554847025
Chanaka Jayarathna <chanaka.j@evicio.com>
2017-02-13 16:15:17 +0530

    IViewLocalizer  Not working Demo
    Signed-off-by: Chanaka Jayarathna <chanaka.j@evicio.com>

--- src/LocalizationDemo/Startup.cs
+++ src/LocalizationDemo/Startup.cs
@@ -7,13 +7,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using Microsoft.Extensions.FileProviders;
 
 namespace LocalizationDemo
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IHostingEnvironment env, IServiceProvider serviceProvider)
         {
+            this.ServiceProvider = serviceProvider;
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -23,12 +30,58 @@ namespace LocalizationDemo
         }
 
         public IConfigurationRoot Configuration { get; }
+        protected IServiceProvider ServiceProvider { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddMvc().AddViewLocalization(
+                LanguageViewLocationExpanderFormat.Suffix,
+                opts => { opts.ResourcesPath = "Resources"; }).
+                AddDataAnnotationsLocalization();
+
+            //// Configure supported cultures and localization options
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("es"),
+                };
+
+                //// State what the default culture for your application is. This will be used if no specific culture
+                //// can be determined for a given request.
+                options.DefaultRequestCulture = new RequestCulture("en-US", "en-US");
+
+                //// You must explicitly state which cultures your application supports.
+                //// These are the cultures the app supports for formatting numbers, dates, etc.
+                options.SupportedCultures = supportedCultures;
+
+                //// These are the cultures the app supports for UI strings, i.e. we have localized resources for.
+                options.SupportedUICultures = supportedCultures;
+            });
+
             // Add framework services.
-            services.AddMvc();
+            var assembly = typeof(SampleResources.SampleResourcesClass).GetTypeInfo().Assembly;
+
+            var fileProviders = new IFileProvider[] {
+                this.ServiceProvider.GetService<IHostingEnvironment>().WebRootFileProvider
+            };
+            var providers= new CompositeFileProvider(fileProviders.Concat(new List<EmbeddedFileProvider> {
+                new EmbeddedFileProvider(assembly, assembly.GetName().Name)
+            }));
+            this.ServiceProvider.GetService<IHostingEnvironment>().WebRootFileProvider = providers;
+
+
+            var mvcBuilder = services.AddMvc();
+            mvcBuilder.AddApplicationPart(assembly);
+            mvcBuilder.AddRazorOptions(
+             o =>
+             {
+                 o.FileProviders.Add(new EmbeddedFileProvider(assembly, assembly.GetName().Name));
+             }
+           );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +99,9 @@ namespace LocalizationDemo
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
 
             app.UseStaticFiles();
 
---
+++ src/SampleResources/Controllers/ExternalResourceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleResources.Controllers
+{
+    public class ExternalResourceController : Controller
+    {
+        private readonly IStringLocalizer<ExternalResourceController> localizer;
+
+        public ExternalResourceController(IStringLocalizer<ExternalResourceController> localizer)
+        {
+            this.localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ControllerLocalizer = this.localizer["ExternalResourceController"];
+            return View();
+        }
+    }
+}
